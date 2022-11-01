fc4ab0309836273ea9446f7813e576c6fd7f786a
Mohit Kumar Raj Badi <42116039+mohitkumarrajbadi@users.noreply.github.com>
2022-11-01 14:56:03 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+function SideBar() {}
+
+ReactDom.render(
+  <div>
+    <h1>Hello this is Mohit</h1>
+  </div>,
+  document.getElementById("root")
+);
