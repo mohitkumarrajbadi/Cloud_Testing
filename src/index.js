56d69d010744393b8cdc11e0ddedf84a88c166bd
Mohit Kumar Raj Badi <mohitkumarrajbadi@gmail.com>
2022-11-01 09:36:06 +0000

    Added the Navbar Section

--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-function SideBar() {}
+function NavBar() {
+  return (
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <div class="container-fluid">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">
+              Active
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Link
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Link
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">
+              Disabled
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
 
-ReactDom.render(
-  <div>
-    <h1>Hello this is Mohit</h1>
-  </div>,
-  document.getElementById("root")
-);
+function Section1(){
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+ReactDom.render(<NavBar />, document.getElementById("root"));
